a50f7cbc7cfc6e407a65bd8ba6ec47dd8d891371
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-08 16:50:08 +0000

    Create function inserting new node as right child of another node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insert new node as left-child.
+ * @parent: Pointer to parent node to create.
+ * @value: int to put store in node.
+ * Return: If success newNode or NULL if failed.
+  */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newNode = binary_tree_node(parent, value);
+
+	if (newNode == NULL)
+		return (NULL);
+
+	/* Check if parent have already left son */
+	if (parent->right != NULL)
+	{
+		/* Update newNode - parent Relation */
+		newNode->right = parent->right;
+		/* Insert newNode as left son */
+		parent->right->parent = newNode;
+	}
+	parent->right = newNode;
+
+	return (newNode);
+}
