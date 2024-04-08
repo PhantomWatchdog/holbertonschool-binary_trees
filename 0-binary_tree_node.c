c9bc139a5f962b9214f8f46e660a0d8fa5a4d53d
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-08 12:41:00 +0000

    Change placement for struct elements

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,15 +13,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	newNode = malloc(sizeof(binary_tree_t));
 
-	newNode->n = value;
-	newNode->parent = parent;
-	newNode->left = NULL;
-	newNode->right = NULL;
-
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
 	return (newNode);
 }
