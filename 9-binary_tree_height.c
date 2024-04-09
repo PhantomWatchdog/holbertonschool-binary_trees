99a9b3da599c7699180d701ca84915cb81e87dc8
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-09 12:39:07 +0000

    Add function measuring the height of a inary tree, made by Tony

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,33 +1,44 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - a function that measures the height of a binary tree.
- * @tree: a pointer to the root node of the tree to traverse.
- * Return: the height of the tree or 0 if tree is NULL.
+ * binary_tree_height - Check the height of a binary tree.
+ * @tree: The root node of the tree to traverse.
+ * Return: Height of tree or 0 if tree is NULL.
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
-
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
-	}
+		size_t left = 0;
+		size_t right = 0;
 
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
+		if (tree->left != NULL)
+		{
+			left = 1 + binary_tree_height(tree->left);
+		}
+		else
+		{
+			left = 0;
+		}
 
-	if (tree->left >= tree->right)
-	{
-		height = binary_tree_height(tree->left);
-	}
-	else
-	{
-		height = binary_tree_height(tree->right);
-	}
+		if (tree->right != NULL)
+		{
+			right = 1 + binary_tree_height(tree->right);
+		}
+		else
+		{
+			right = 0;
+		}
 
-	return (1 + height);
+		if (left > right)
+		{
+			return (left);
+		}
+		else
+		{
+			return (right);
+		}
+	}
+	return (0);
 }
