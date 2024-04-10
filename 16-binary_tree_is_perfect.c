b33286f4302cc5c83a13400543fcd5f9d3f36973
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-10 13:13:58 +0000

    Test V2 for task 16 check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,20 +27,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int leftHeight = binary_tree_height(tree->left),
-	    rightHeight = binary_tree_height(tree->right);
+	if (tree)
+	{
+		int leftHeight = binary_tree_height(tree->left),
+		    rightHeight = binary_tree_height(tree->right);
 
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+
+		if (leftHeight != rightHeight)
+		{
+			return (0);
+		}
+	}
 	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-
-	if (leftHeight != rightHeight)
 	{
 		return (0);
 	}
