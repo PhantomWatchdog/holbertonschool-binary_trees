5547d8d76c270f105082416db759d8c5d9b12326
TonyHolby <tonynemoutheholberton@gmail.com>
2024-04-10 08:12:13 +0000

    Fix code in binary_tree_depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,19 +9,17 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
 	if (tree != NULL)
 	{
 		if (tree->parent != NULL)
 		{
-			depth = 1 + binary_tree_depth(tree->parent);
+			return (1 + binary_tree_depth(tree->parent));
 		}
 		else
 		{
-			depth = 0;
+			return (0);
 		}
 	}
 
-	return (depth);
+	return (0);
 }
