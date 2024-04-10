f10158f695a931156c848f3d7e49d41d77c77b4d
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-10 08:57:15 +0000

    Optimization and testing of if statements

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,19 +8,18 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree)
 	{
-		return (1);
-	}
+		if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		{
+			return (1);
+		}
 
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-
-	if (tree->left == NULL || tree->right == NULL)
-	{
-		return (0);
+		/* Two child detection */
+		if (tree->left == NULL || tree->right == NULL)
+		{
+			return (0);
+		}
 	}
 
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
